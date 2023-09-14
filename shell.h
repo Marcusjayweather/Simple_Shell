a8cd865dbf1c5917c19a2ef5691e00268146d208
marcusjayweather <marcusjnr61@gmail.com>
2023-09-14 09:31:11 -0700

    marcusjunior

--- shell.h
+++ shell.h
@@ -13,19 +13,27 @@
 #include <errno.h>
 
 /* read/write buffers */
+
 #define READ_BUF_SIZE 1024
 #define WRITE_BUF_SIZE 1024
 #define BUF_FLUSH -1
+
 /* convert_number */
+
 #define CONVERT_LOWERCASE	1
 #define CONVERT_UNSIGNED	2
+
 /* 1 if using system getline() */
+
 #define USE_GETLINE 0
 #define USE_STRTOK 0
 #define HIST_FILE	".simple_shell_history"
 #define HIST_MAX	4096
+
 extern char **environ;
+
 /* command chaining */
+
 #define CMD_NORM	0
 #define CMD_OR		1
 #define CMD_AND		2
@@ -205,6 +213,7 @@ int replace_string(char **, char *);
  *@readfd: the fd from which to read line input
  *@histcount: the history line number count
  */
+
 typedef struct passinfo
 {
 	char *fname;
